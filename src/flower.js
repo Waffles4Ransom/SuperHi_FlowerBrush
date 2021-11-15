488a9dd8a342a7e8dd1dad86fb6de88993787820
Jenna Ransom <jenna.ransom@gmail.com>
2021-11-14 18:00:30 -0700

    playing with fading and resizing the drawing

--- src/flower.js
+++ src/flower.js
@@ -4,12 +4,15 @@ class Flower {
     this.y = y
     this.hue = hue
     this.midSize = random(20, 40)
+    this.alpha = 100
   }
 
   draw() {
     colorMode(HSB, 100)
     noStroke()
-    fill(this.hue, 100, 100)
+    fill(this.hue, 100, 100, this.alpha)
     circle(this.x, this.y, this.midSize)
+    this.alpha -= 1
+    this.midSize += 1
   }
 }
